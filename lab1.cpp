bda52e014881aa4dd4c5b9d28e0b6ae09d7d47a0
happy210168 <happy@gmail.com>
2018-03-11 16:22:09 +0800

    <lab1> happy210168 -- lab1

--- lab1.cpp
+++ lab1.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+	int num;
+	cin >> num;
+	cout << num <<' ';
+	while(num!=1){
+		if(num%2){
+			num = num*3 + 1;
+		}
+		else num = num/2;
+		cout << num <<' ';
+	}	
 	
-
-
-
-
 
 	return 0;
 }
