d5099f29d720858734338be30ce2aa80ee3c7fe9
happy210168 <happy@gmail.com>
2018-03-11 16:44:32 +0800

    <lab1> update lab1

--- lab1.cpp
+++ lab1.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main(){
 	int num;
 	cin >> num;
+	if(num<1){
+		cout << "please type positive integer" <<'\n';
+		return 0;
+	}
 	cout << num <<' ';
 	while(num!=1){
 		if(num%2){
@@ -11,7 +15,5 @@ int main(){
 		else num = num/2;
 		cout << num <<' ';
 	}	
-	
-
 	return 0;
 }
